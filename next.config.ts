327b9418c0c9a643d9afe1cdcb78463c13be7e22
Sanyam Verma <sanyamverma274@gmail.com>
2025-10-07 13:50:26 +0000

    https://tradewatch10.vercel.app/sitemap.xml also it is showing 404 error

--- next.config.ts
+++ next.config.ts
@@ -43,7 +43,8 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
        {
-        protocol: 'https',
+        protocol: 'https'
+        ,
         hostname: 'img.etimg.com',
         port: '',
         pathname: '/**',
